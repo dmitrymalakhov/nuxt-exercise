908b3e129a6b8463204b3f258933e703b079d8c4
Dmitry Malakhov <dmitrii.malakhov@gmail.com>
2022-05-17 20:24:52 +0300

    feat(layout): added default app layout

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,4 @@
 export default {
-  /*
-   ** Headers of the page
-   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
-   */
   head: {
     title: "Nuxt.js starter for CSB",
     meta: [
@@ -16,25 +12,8 @@ export default {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
-  /*
-   ** Nuxt.js modules
-   ** Doc: https://modules.nuxtjs.org
-   */
   modules: [],
-
-  /*
-   ** Global CSS
-   ** Doc: https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-css
-   */
-  css: [],
-
-  /*
-   ** Plugins to load before mounting the App
-   ** Doc: https://nuxtjs.org/docs/2.x/directory-structure/plugins
-   */
+  css: ["~/assets/css/main"],
   plugins: []
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,43 @@
 <template>
-  <Tutorial />
+  <div class="grid">
+    <div class="column"></div>
+    <div class="column content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nec
+      blandit orci. Etiam tincidunt ex et posuere suscipit. Pellentesque vitae
+      orci pharetra nibh efficitur consectetur. Nulla commodo bibendum tortor,
+      eget sagittis ipsum porttitor quis. Aliquam sollicitudin posuere elit,
+      vestibulum sagittis ex ultrices non. Quisque vulputate laoreet nibh, ut
+      efficitur dolor pharetra et. Vivamus id dignissim sapien, at pulvinar
+      eros. Quisque aliquam porta est, quis bibendum elit faucibus iaculis. Sed
+      ut vulputate ipsum. Vivamus nec luctus nulla, in tincidunt purus. Integer
+      eget justo odio. Duis auctor, elit a gravida pulvinar, enim erat
+      vestibulum mi, vel blandit leo felis quis lectus. Phasellus non ultrices
+      nulla. Phasellus accumsan lobortis justo, dignissim convallis ante
+      malesuada ut. Vestibulum aliquam neque at maximus maximus. Sed semper enim
+      ut magna lacinia blandit. Nam gravida quam leo, vel lacinia ligula
+      bibendum at. Vestibulum at augue nec quam sagittis scelerisque. Nullam
+      posuere nulla a turpis pellentesque interdum et id leo. Pellentesque urna
+      tortor, dignissim et euismod sed, gravida mollis dolor. Nam quis accumsan
+      eros. Phasellus ac pharetra velit, at bibendum enim. Nulla efficitur vitae
+      justo at pulvinar. In et dui a augue tincidunt tincidunt sed sed ante.
+      Morbi et libero lectus. Suspendisse potenti. Vivamus massa magna,
+      facilisis sed sollicitudin maximus, placerat eu nibh. Mauris ac enim
+      mollis, elementum nisi nec, viverra augue. Proin erat neque, maximus et
+      leo eget, pellentesque iaculis elit. Aenean faucibus nulla ac lectus
+      dictum facilisis. Nam consequat tortor sit amet mollis tempor.
+    </div>
+    <div class="column"></div>
+  </div>
 </template>
+
+<style scoped>
+.grid {
+  padding-top: 86px;
+  display: grid;
+  grid-template-columns: 1fr minmax(400px, 564px) 1fr;
+  grid-column-gap: 10px;
+}
+.content {
+  background: #FFFFFF;
+}
+</style>
